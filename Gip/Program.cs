fbc7bd179474ca444953056bdd13061973e86b91
Thomas Claes <r0664186@ucll.be>
2020-02-20 14:06:38 +0000

    Updated Program.cs

--- Gip/Program.cs
+++ Gip/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Gip1
+namespace Gip
 {
     public class Program
     {
