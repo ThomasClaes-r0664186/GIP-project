b5fbaf16a974c3ad1c43c9fdd6895a60aa6d5a6b
Thomas Claes <r0664186@ucll.be>
2020-02-20 14:06:27 +0000

    Updated Startup.cs

--- Gip/Startup.cs
+++ Gip/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Gip1
+namespace Gip
 {
     public class Startup
     {
