923e9b2f521cb660e4489d83071da704e5bd5c7d
Jaimie Haesevoets <r0800449@ucll.be>
2020-02-24 16:18:59 +0100

    Middelen terug veranderd naar hoe het eerst was

--- Gip/Models/Room.cs
+++ Gip/Models/Room.cs
@@ -10,19 +10,40 @@ namespace Gip.Models
             CourseMoment = new HashSet<CourseMoment>();
         }
 
-        public string Gebouw { get; set; }
+        public string Gebouw { get; set; } //if(value.toLower.trim().length <= 0) throw new exeption, else gebouw = value
         public int Verdiep { get; set; }
         public string Nummer { get; set; }
         public string Type { get; set; }
         public int Capaciteit { get; set; }
-        public int ProjectorSetup { get; set; }
-        public bool Scherm { get; set; }
-        public bool Wifi { get; set; }
-        // middelen is veranderd naar --> int projectorSetup, bool scherm, bool wifi
-        /// <summary>
-        /// getter en setters voor alles inplementeren: bijvoorbeeld mail mag niet leeg zijn of moet @ bevatten
-        /// exceptions aanmaken om dan te gooien       
-        /// </summary>
+       // public string Middelen { get; set; }
+        //get splitst middelen op en returned utils object -->(nakijken in die string of er projectorsetup,... return true else false)
+        //opzoeken hoe je string met / 
+
+        private String middelen;
+
+
+
+        public String Middelen
+        {
+            get
+            {
+                return middelen;
+
+                /*  bool Projectorsetup;
+                bool Schermen;
+                bool Wifi;
+                if (middelen =="/Projectorsetup") //
+                {
+                    Projectorsetup = true;
+                }*/
+            }
+            set
+            { 
+                middelen = value;
+            }
+            
+        }
+
 
         public virtual ICollection<CourseMoment> CourseMoment { get; set; }
     }
---
+++ Gip/Models/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gip.Models
+{
+    public class Utils
+    {
+        public bool Projector { get; set; }
+        public bool Scherm { get; set; }
+        public bool Wifi { get; set; }
+    }
+}
