0ec21b8ae3895bef2c3f6896f2941c7e569da211
Jaimie Haesevoets <r0800449@ucll.be>
2020-02-25 13:15:52 +0100

    In de Room klasse zijn er getters en setters gemaakt voor elke property. In de Utils klasse is  een nieuwe property "Schermen" gemaakt omdat deze ontbrak. exception klasse gemaakt voor room.

---
+++ Gip/Models/Exceptions/RoomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gip.Models.Exceptions
+{
+    public class RoomException : Exception
+    {
+        public RoomException()
+        {
+            
+        }
+        public RoomException(string message) : base(message)
+        {
+            
+        }
+    }
+}
