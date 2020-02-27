3ad75a6f16ebcf3bfd494a984721a46cd07c9299
snakehead007 <snakehead007@protonmail.com>
2020-02-27 16:14:11 +0100

    added selenium

---
+++ GipUnitTest/SeleniumTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace GipUnitTest
+{
+    
+    public class SeleniumTest : IDisposable
+    {
+        public IWebDriver driver;
+        string poortNummer = "5001";
+        
+        public SeleniumTest()
+        {
+            try
+            {
+                driver = new ChromeDriver();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        [Fact]
+        public void TitleOfPageShouldBeCorrect()
+        {
+            driver.Navigate().GoToUrl("localhost:"+poortNummer+"/");
+            Assert.Equals("Home Page - Gip", driver.Title);
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
