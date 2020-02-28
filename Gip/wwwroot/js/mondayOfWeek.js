5e03be53b3c27de74d2c81ea614c257311c0c721
Joni Timmermans <r0800510@ucll.be>
2020-02-28 14:18:47 +0100

    Small changes

--- Gip/wwwroot/js/mondayOfWeek.js
+++ Gip/wwwroot/js/mondayOfWeek.js
@@ -1,1 +1,10 @@
-﻿
+﻿var curr = new Date;
+var first = curr.getDate() - curr.getDay();
+var first = first + 1
+var last = first + 4;
+
+var monday = new Date(curr.setDate(first)).toLocaleDateString();
+var friday = new Date(curr.setDate(last)).toLocaleDateString();
+
+document.getElementById("dateOfWeek").innerHTML = "Planning van week " + monday + " tot " + friday;
+
