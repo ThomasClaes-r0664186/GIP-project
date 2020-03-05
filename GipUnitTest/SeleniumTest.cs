697eb0b8e64bce4e00ff0f1de674640379827b07
snakehead007 <snakehead007@protonmail.com>
2020-03-05 12:46:12 +0100

    selenium test

--- GipUnitTest/SeleniumTest.cs
+++ GipUnitTest/SeleniumTest.cs
@@ -8,36 +8,28 @@ using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using Gip;
 namespace GipUnitTest
 {
-    [TestClass]
+    [TestFixture]
     public class SeleniumTest : IDisposable
     {
         public IWebDriver driver;
         string poortNummer = "5001";
-
+    
+        [SetUp]
         public void SetUp()
         {
             Gip.Program.Main(null);
-        }
-        
-        public SeleniumTest()
-        {
             try
             {
                 driver = new ChromeDriver();
-                
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+
             }
         }
-        [Test]
-        public void TitleOfPageShouldBeCorrect()
-        {
-            driver.Navigate().GoToUrl("localhost:"+poortNummer+"/");
-            Assert.Equals("Home Page - Gip", driver.Title);
-        }
-
+        [TearDown]
         public void Dispose()
         {
             try
@@ -48,6 +40,13 @@ namespace GipUnitTest
             {
                 Console.WriteLine(e.Message);
             }
+        }
+    
+        [Test]
+        public void TitleOfPageShouldBeCorrect()
+        {
+            driver.Navigate().GoToUrl("localhost:"+poortNummer+"/");
+            Assert.Equals("Home Page - Gip", driver.Title);
         }
     }
 }
