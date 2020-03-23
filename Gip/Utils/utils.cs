5f6bb61d923198d758347c18de883ea45c906ab4
snakehead007 <snakehead007@pm.me>
2020-03-23 10:32:32 +0100

    add hasher

---
+++ Gip/Utils/utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace Gip.Utils
+{
+    public class utils
+    {
+        public string hashPassword(String plain_text_password)
+        {
+            string password = plain_text_password;
+ 
+            // generate a 128-bit salt using a secure PRNG
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            return hashed;
+        }
+    }
+    
+}
