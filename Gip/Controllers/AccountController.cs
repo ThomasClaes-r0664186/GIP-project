8ee61ca0f4bbd206445cf0ce8bc34ec796f8ddbd
Thomas Claes <r0664186@ucll.be>
2020-03-25 13:52:16 +0100

    fixed shit

--- Gip/Controllers/AccountController.cs
+++ Gip/Controllers/AccountController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Gip.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using RegistrationFormCore.ViewModels;
 
-namespace RegistrationFormCore.Controllers
+namespace Gip.Controllers
 {
     public class AccountController : Controller
     {
--- Gip/Models/RegisterViewModel.cs
+++ Gip/Models/RegisterViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RegistrationFormCore.ViewModels
+namespace Gip.Models
 {
     public class RegisterViewModel
     {
