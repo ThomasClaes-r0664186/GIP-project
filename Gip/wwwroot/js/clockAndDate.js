7caffa99bf1dd3d26e74c10185e1df412019ba0a
Joni Timmermans <r0800510@ucll.be>
2020-03-31 19:54:27 +0200

    Remastered clock

---
+++ Gip/wwwroot/js/clockAndDate.js
@@ -0,0 +1,23 @@
+﻿Number.prototype.pad = function (n) {
+    for (var r = this.toString(); r.length < n; r = 0 + r);
+    return r;
+};
+
+function updateClock() {
+    var now = new Date();
+    var min = now.getMinutes(),
+        hou = now.getHours(),
+        mo = now.getMonth(),
+        dy = now.getDate(),
+        yr = now.getFullYear();
+    var months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+    var tags = ["mon", "d", "y", "h", "m"],
+        corr = [months[mo], dy, yr, hou.pad(2), min.pad(2)];
+    for (var i = 0; i < tags.length; i++)
+        document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+}
+
+function clockDate() {
+    updateClock();
+    window.setInterval("updateClock()", 1);
+}
