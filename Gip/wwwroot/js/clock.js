1923ba4e81fa15416487ae4c1c8469913bd69bf8
Joni Timmermans <r0800510@ucll.be>
2020-04-01 16:32:47 +0200

    Clock and date in footer poging 3000

---
+++ Gip/wwwroot/js/clock.js
@@ -0,0 +1,31 @@
+﻿function digitalClock() {
+
+    let timeAndDateHTML = document.getElementById('dateAndClock');
+
+
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let date = d.getDate();
+    let hours = d.getHours();
+    hours = addZero(hours);
+    let minutes = d.getMinutes();
+    minutes = addZero(minutes);
+    let seconds = d.getSeconds();
+    seconds = addZero(seconds);
+    let today = d.getDay();
+    let dayName = ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'];
+    let monthName = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
+
+    timeAndDateHTML.innerHTML = dayName[today] + ',  ' + date + ' ' + monthName[month] + ' ' + year + ' ' + +hours + ':' + minutes + ':' + seconds;
+
+}
+
+function addZero(i) {
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
+}
+
+setInterval(digitalClock, 1000);
