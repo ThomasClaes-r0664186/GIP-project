0173bfd5e793d99012ece34debaef8da800d9269
Joni Timmermans <r0800510@ucll.be>
2020-04-02 21:36:33 +0200

    Added message based on day, fixed clock positie van fixed naar absolute.

---
+++ Gip/wwwroot/js/messageBasedOnTime.js
@@ -0,0 +1,19 @@
+﻿let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+
+if (hourNow < 12) {
+    greeting = "Goedemorgend";
+}
+else if (hourNow < 20) {
+    greeting = "Goedenamiddag";
+}
+else if (hourNow < 24) {
+    greeting = "Goedenavond";
+}
+else {
+    greeting = "Welkom";
+}
+
+document.getElementById("greeting").innerText = greeting;
