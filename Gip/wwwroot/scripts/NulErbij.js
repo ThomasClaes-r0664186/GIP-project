4086c52b1f1ac99b6222bfcf8ab5bfc4809a967b
Thomas Claes <r0664186@ucll.be>
2020-05-06 15:02:34 +0200

    moved inline script to js files

---
+++ Gip/wwwroot/scripts/NulErbij.js
@@ -0,0 +1,12 @@
+﻿function nulErbij() {
+    var val = document.getElementById('nummer').value;
+    var numbers = Number(val.replace(/[a-zA-Z]/g, ''));
+    var letter = val.replace(/[0-9]/g, '');
+    if (val !== "") {
+        var letter = val.replace(/[0-9]/g, '');
+        var numbers = Number(val.replace(/[a-zA-Z]/g, ''));
+        if (numbers.toString().length === 1 && val !== 0) {
+            document.getElementById('nummer').value = "0" + numbers + letter;
+        }
+    }
+}
