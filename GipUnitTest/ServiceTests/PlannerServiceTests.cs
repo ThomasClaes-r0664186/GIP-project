83e695e4a7682131ae5cf0f64dd5daa5f95cc539
Thomas Claes <r0664186@ucll.be>
2020-06-01 16:25:47 +0200

    fixing delete from push

--- GipUnitTest/ServiceTests/PlannerServiceTests.cs
+++ GipUnitTest/ServiceTests/PlannerServiceTests.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GipUnitTest.ServiceTests
@@ -33,5 +34,159 @@ namespace GipUnitTest.ServiceTests
         }
 
         //
+
+
+        [TestMethod]
+        public void GetPlanningLectAdminTest() 
+        {
+            /*
+            // ARRANGE
+            Course course = new Course { Vakcode = "", Titel = "", Studiepunten = 0};
+            ctxDb.Course.Add(course);
+            
+            Room room = new Room { Gebouw = "", Verdiep = 0, Nummer = "", Type = "", Capaciteit = 20};
+            ctxDb.Room.Add(room);
+
+            Schedule schedule = new Schedule { Datum = new DateTime(2020, 06, 5), Startmoment = new DateTime(0, 0, 0, 11, 0, 0), Eindmoment = new DateTime(0, 0, 0, 13, 0, 0) };
+            ctxDb.Schedule.Add(schedule);
+
+            ApplicationUser user = new ApplicationUser { };
+            ctxDb.Users.Add(user);
+
+            ctxDb.SaveChanges();
+
+            int courseId = ctxDb.Course.Where(c => c.Vakcode == "").FirstOrDefault().Id;
+            int roomId = ctxDb.Room.Where(r => r.Gebouw == "" & r.Verdiep == 0 & r.Nummer == "").FirstOrDefault().Id;
+            int scheduleId = ctxDb.Schedule.Where(s => s.Datum == new DateTime(0, 0, 0)).FirstOrDefault().Id;
+            string userId = ctxDb.Users.Where(u => u.UserName == "").FirstOrDefault().Id;
+            
+            CourseMoment cm = new CourseMoment { };
+            
+            // ACT
+            
+            // ASSERT
+            */
+        }
+
+        [TestMethod]
+        public void AddPlanningTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void GetLokalenTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void DeletePlanningTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void EditPlanningTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void GetTopicTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void GetCourseMomentsTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void GetStudsInCmTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void EditStudsInCmTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void GetCourseUsersTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void GetStudsInEachCmTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
+
+        [TestMethod]
+        public void AddStudsToEachCmTest() 
+        {
+            // ARRANGE
+
+            // ACT
+            
+            // ASSERT
+            
+        }
     }
 }
