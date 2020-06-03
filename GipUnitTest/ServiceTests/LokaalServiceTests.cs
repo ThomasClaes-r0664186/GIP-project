885e57ef39fa5f841d5067e17cc7f759599c973a
jaimie haesevoets <jaimiehaesevoets@hotmail.com>
2020-06-03 15:14:21 +0200

    commentaar geschreven in LectorServiceTest EditLokaalTest en DeleteLokaalTest geschreven

--- GipUnitTest/ServiceTests/LokaalServiceTests.cs
+++ GipUnitTest/ServiceTests/LokaalServiceTests.cs
@@ -38,12 +38,62 @@ namespace GipUnitTest.ServiceTests
         [TestMethod]
         public void AddLokaalTest()
         {
+            // ARRANGE
             LokaalService service = new LokaalService(ctxDb);
+            
+            // ACT
             service.AddLokaal("A", 2, "01", "Vergaderlokaal", 5, "Geen middelen");
-
             Room room = ctxDb.Room.Where(c => c.Gebouw.Equals("A")).FirstOrDefault();
+
+            // ASSERT
+            Assert.IsNotNull(room);
             Assert.IsTrue(room.Gebouw == "A");
+            Assert.IsTrue( room.Verdiep == 2);
+            Assert.IsTrue(room.Nummer == "01");
         }
 
+        [TestMethod]
+        public void DeleteLokaalTest()
+        {
+            // ARRANGE
+            LokaalService service = new LokaalService(ctxDb);
+            service.AddLokaal("A", 2, "01", "Vergaderlokaal", 5, "Geen middelen");
+            int lId = ctxDb.Room.Where(i => i.Verdiep.Equals(2)).FirstOrDefault().Id;
+            
+            // ACT
+            service.DeleteLokaal(lId);
+
+            // ASSERT
+            Assert.IsTrue(!ctxDb.Room.Any());
+        }
+
+        [TestMethod]
+        public void EditLokaalTest()
+        {
+            // ARRANGE
+            LokaalService service = new LokaalService(ctxDb);
+            service.AddLokaal("A", 2, "01", "Vergaderlokaal", 5, "Geen middelen");
+            int lId = ctxDb.Room.Where(i => i.Verdiep.Equals(2)).FirstOrDefault().Id;
+            string gebouw = "B";
+            //verdiep blijft hetzelfde
+            string nummer = "02";
+            string type = "Aula";
+            int capac = 10;
+            string middelen = "Scherm";
+
+            // ACT 
+            service.EditLokaal(lId, gebouw, 2, nummer, type, capac, middelen);
+
+            // ASSERT
+            Room room = ctxDb.Room.Find(lId);
+            Assert.IsTrue(room.Gebouw.Equals("B"));
+            Assert.IsTrue(room.Verdiep ==2);
+            Assert.IsTrue(room.Nummer.Equals("02"));
+            Assert.IsTrue(room.Type.Equals("Aula"));
+            Assert.IsTrue(room.Capaciteit == 10);
+            Assert.IsTrue(room.Middelen.Equals("Scherm"));
+           
+
+        }
     }
 }
